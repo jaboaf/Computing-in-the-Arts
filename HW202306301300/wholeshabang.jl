9d97ec469979de3e33ea816480d9b04b7dbe00e0
jaboaf <50273977+jaboaf@users.noreply.github.com>
2023-07-02 20:30:49 -0400

    notes and hw stuff

--- HW202306301300/wholeshabang.jl
+++ HW202306301300/wholeshabang.jl
@@ -2,4 +2,8 @@ F = filter(x->x[12]!='.',readdir("Falling Up",join=true))
 poems = read.(F) #::Vector{Vector{UInt8}}
 alphabet = sort(union(poems...)) #::Vector{UInt8}
 write("alphabet.txt",alphabet);
+for poem in poems
+
+i = findfirst(0x0a,poem)
+push!(lines, poem[1:i-1])
 
